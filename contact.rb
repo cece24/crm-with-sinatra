46f5d55dd73a3bb6cc9efce80d48fa0b78c7b4d5
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-03 17:36:47 -0400

    Set up for assignment, creating sqlite3 database, crm file and views folder

--- contact.rb
+++ contact.rb
@@ -18,11 +18,3 @@ class Contact < ActiveRecord::Base
 end
 
 Contact.auto_upgrade!
-
-# new_contact = Contact.create("Cece", "Wong", "cece@gmail.com", "likes kittens")
-# new_contact_2 = Contact.create("Jane", "Gomez", "janey@gmail.com", "is a writer")
-# new_contact_3 = Contact.create("Brett", "Cecil", "brett@gmail.com", "plays baseball")
-#
-# puts Contact.all.inspect
-# new_contact.update
-# puts new_contact.inspect
