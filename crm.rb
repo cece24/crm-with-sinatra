cc8e8e853b8b3bcc34c4dddd7704b8e483c2b726
Cecilia Wong <ceciliakhwong@gmail.com>
2017-10-05 15:18:49 -0400

    Add post method which creates a new Contact in the database

--- crm.rb
+++ crm.rb
@@ -27,6 +27,16 @@ get "/contacts/:id" do
   end
 end
 
+post "/contacts" do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to("/contacts")
+end
+
 
 after do
   ActiveRecord::Base.connection.close
